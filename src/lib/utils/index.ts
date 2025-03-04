8a33b1953273e72f002da7abf05db4657322e307
Leno <lenin.an.ln@gmail.com>
2025-03-04 03:30:08 -0400

    feat: add util functions for time string conversions

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,14 +1,23 @@
 export const formatFromSeconds = (seconds: number) => {
-  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
   const remainingSeconds = seconds % 60;
-  return `${String(minutes).padStart(2, "0")}:${String(
-    remainingSeconds
-  ).padStart(2, "0")}`;
+  const finalTime = `${String(hours).padStart(2, "0")}:${String(
+    minutes
+  ).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
+  console.log(finalTime);
+  return finalTime;
 };
 
-/// Format MM:SS to seconds
+/// Formats HH:MM:SS to seconds
 
-export const formatToSeconds = (time: string) => {
-  const [minutes, seconds] = time.split(":");
-  return parseInt(minutes) * 60 + parseInt(seconds);
+export const formatHMSToSeconds = (time: string) => {
+  const [hours = "00", minutes = "00", seconds = "00"] = time.split(":");
+
+  console.log("raw strings, formatHMSToSeconds", hours, minutes, seconds);
+  console.log(
+    "Formatted time to seconds:",
+    parseInt(hours) * 3600 + parseInt(minutes) * 60 + parseInt(seconds)
+  );
+  return parseInt(hours) * 3600 + parseInt(minutes) * 60 + parseInt(seconds);
 };
